ea51b6b521e0a9a69929efa3b60180aa6893b8c5
Gertian Mici <micigentian1@gmail.com>
2026-09-19 15:55:35 +0200

    Add billable day counting for a rental period

--- PsychicFiesta/Domain/RentalPeriod.cs
+++ PsychicFiesta/Domain/RentalPeriod.cs
@@ -1,6 +1,19 @@
 namespace PsychicFiesta.Domain;
 
-public class RentalPeriod
+public static class RentalPeriod
 {
-    
+    public static int BillableDays(DateTimeOffset pickedUpAt, DateTimeOffset returnedAt)
+    {
+        if (returnedAt < pickedUpAt)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(returnedAt),
+                message: "Return time cannot be earlier than pickup time");
+        }
+
+        long ticks = (returnedAt - pickedUpAt).Ticks;
+        long wholeDays = (ticks / TimeSpan.TicksPerDay);
+        int partialDay = (ticks % TimeSpan.TicksPerDay) == 0 ? 0 : 1;
+        return Math.Max(1, checked((int)(wholeDays + partialDay)));
+    }
 }
