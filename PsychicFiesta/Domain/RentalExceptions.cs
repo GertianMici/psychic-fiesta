079aaa7f7715c6cc759e8c09103951941e08b64f
Gertian Mici <micigentian1@gmail.com>
2026-09-19 15:55:39 +0200

    Add rental domain exceptions

--- PsychicFiesta/Domain/RentalExceptions.cs
+++ PsychicFiesta/Domain/RentalExceptions.cs
@@ -1,6 +1,16 @@
 namespace PsychicFiesta.Domain;
 
-public class RentalExceptions
-{
-    
-}
+public sealed class DuplicateBookingException(BookingNumber bookingNumber)
+    : InvalidOperationException($"Booking {bookingNumber} is already in use.");
+
+public sealed class RentalNotFoundException(BookingNumber bookingNumber)
+    : InvalidOperationException($"Booking {bookingNumber} is not found.");
+
+public sealed class RentalAlreadyReturnedException(BookingNumber bookingNumber)
+    : InvalidOperationException($"Booking {bookingNumber} has been returned.");
+
+public sealed class RentalConcurrencyException(BookingNumber bookingNumber)
+    : InvalidOperationException($"Booking {bookingNumber} changed. Try to reload.");
+
+public sealed class UnsupportedCategoryException(CarCategory category)
+    : InvalidOperationException($"No price is configured for category {category}.");
